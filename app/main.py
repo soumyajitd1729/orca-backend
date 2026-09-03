f70afa8de369c8525e9aa5db9f6da12f52c74013
soumyajitd1729 <soumyajitd6174@gmail.com>
2026-09-04 00:34:07 +0530

    update CORS

--- app/main.py
+++ app/main.py
@@ -77,7 +77,7 @@ cors_origins = [origin.strip() for origin in settings.CORS_ORIGINS.split(",") if
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=cors_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
